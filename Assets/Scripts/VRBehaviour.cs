2cc62ad2f68ee082cbaa4eaf0336600ef165c20a
LeLouw21 <6000176@sae.edu.za>
2024-05-17 10:57:30 +0200

    VRBehaviour || Inst. Timer Added

--- Assets/Scripts/VRBehaviour.cs
+++ Assets/Scripts/VRBehaviour.cs
@@ -4,27 +4,57 @@ using UnityEngine;
 
 public class VRBehaviour : MonoBehaviour
 {
+    //prefab you want to instantiate
     public GameObject destroyedPrefab;
+
+    private Vector3 startPos;
+
+    private bool isDestoyed;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
+        Debug.Log(startPos);
+
+        isDestoyed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //SpawnObject(startPos);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
+            //instantiate prefab
             GameObject deadAsset = Instantiate(destroyedPrefab, transform.position, transform.rotation);
+
+            float count = 0;
+            count += Time.deltaTime;
+            if(count >= 7)
+            {
+                SpawnObject(startPos);
+            }
+
             Destroy(this.gameObject);
             Destroy(deadAsset, 7);
+
+            //WaitForSeconds(7);
+            isDestoyed = true;
+        }
+    }
+
+    private void SpawnObject(Vector3 start)
+    {
+        if(isDestoyed)
+        {
+            Debug.Log("Inactive, respwan running");
+
+            Instantiate(gameObject, start.normalized, Quaternion.identity);
         }
     }
 }
