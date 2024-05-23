039e7596f864707692050f39c3e2c4de01c6a993
veryReallyGoodCoder <6000176@sae.edu.za>
2024-05-23 08:54:24 +0200

    MainScene || Enemy AI Added

--- Assets/GameJam/Scripts/EnemyScript.cs
+++ Assets/GameJam/Scripts/EnemyScript.cs
@@ -10,6 +10,7 @@ public class EnemyScript : MonoBehaviour
     public Transform player;
 
     [SerializeField] private float speed;
+    [SerializeField] private float distToPlayer;
     
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,18 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        //calculate distance from pig to player
+        distToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if(distToPlayer >= 0.1)
+        {
+            agent.destination = player.position;
+            //Debug.Log("Chasing Player");
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+
     }
 }
